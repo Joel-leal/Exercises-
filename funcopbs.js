f36f263c287d91285daecc17bc509a86ff1c9783
Joel-leal <joel.lasi@hotmail.com>
2021-01-28 20:57:59 -0300

    enuciado do exercício

---
+++ funcopbs.js
@@ -0,0 +1,16 @@
+function sum(x,y){
+    return x + y
+}
+function multiplique(x,y){
+    return x * y
+}
+function sub(x,y) {
+    return x - y
+}
+function div(x,y)  {
+    return x/y
+}
+console.log(sum(3,2))
+console.log(multiplique(3,2))
+console.log(sub(3,2))
+console.log(div(3,2))
